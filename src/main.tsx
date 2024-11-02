f9f4b06e94a738d789b6c0003d93335aa291d76e
ItsKalfar <gundsagar02@gmail.com>
2024-11-02 17:48:26 +0530

    completed the setup of redux and tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import ThemeProviderWrapper from "./components/theme-provider.tsx";
+import { Provider } from "react-redux";
+import store, { persistor } from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ReactQueryProvider } from "@/react-query/react-query-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProviderWrapper>
-      <App />
-    </ThemeProviderWrapper>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ReactQueryProvider>
+          <ThemeProviderWrapper>
+            <App />
+          </ThemeProviderWrapper>
+        </ReactQueryProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
---
+++ src/store/index.ts
@@ -0,0 +1,8 @@
+import store from "./storeSetup";
+
+export * from "./hooks";
+export * from "./rootReducer";
+export * from "./slices/auth";
+export * from "./storeSetup";
+
+export default store;
